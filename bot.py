583c6c38326e31301fa96d6fde0cd351c0063dd2
Valerii Voronkov <scorpirat@gmail.com>
2021-06-14 12:14:13 +0300

    Add json and pprint libraries to the project.

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import websocket
+import websocket, json, pprint
 
 # from github https://github.com/binance/binance-spot-api-docs/blob/master/web-socket-streams.md#general-wss-information
 SOCKET = "wss://stream.binance.com:9443/ws/dogeusdt@kline_1m"
@@ -10,7 +10,9 @@ def on_close_func(ws):
     print('the connection has closed')
 
 def on_message_func(ws, message):
-    print('the message has sent')
+    # print(message)
+    json_message = json.loads(message)
+    pprint.pprint(json_message)
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open_func, on_close=on_close_func, on_message=on_message_func)
 ws.run_forever()
